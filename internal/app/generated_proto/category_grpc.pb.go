f4d6030e99021492d33677d3bf0004a184966f9f
arrazy100 <afdhalarrazy111@gmail.com>
2024-08-18 21:15:30 +0700

    Implement List, Edit, Detail, Delete Category

--- internal/app/generated_proto/category_grpc.pb.go
+++ internal/app/generated_proto/category_grpc.pb.go
@@ -20,6 +20,10 @@ const _ = grpc.SupportPackageIsVersion9
 
 const (
 	CategoryService_CreateCategory_FullMethodName = "/pft.CategoryService/CreateCategory"
+	CategoryService_ListCategories_FullMethodName = "/pft.CategoryService/ListCategories"
+	CategoryService_EditCategory_FullMethodName   = "/pft.CategoryService/EditCategory"
+	CategoryService_GetCategory_FullMethodName    = "/pft.CategoryService/GetCategory"
+	CategoryService_DeleteCategory_FullMethodName = "/pft.CategoryService/DeleteCategory"
 )
 
 // CategoryServiceClient is the client API for CategoryService service.
@@ -27,6 +31,10 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type CategoryServiceClient interface {
 	CreateCategory(ctx context.Context, in *CreateCategoryRequest, opts ...grpc.CallOption) (*CreateCategoryResponse, error)
+	ListCategories(ctx context.Context, in *ListCategoryRequest, opts ...grpc.CallOption) (*ListCategoryResponse, error)
+	EditCategory(ctx context.Context, in *Category, opts ...grpc.CallOption) (*Category, error)
+	GetCategory(ctx context.Context, in *GetCategoryRequest, opts ...grpc.CallOption) (*Category, error)
+	DeleteCategory(ctx context.Context, in *DeleteCategoryRequest, opts ...grpc.CallOption) (*DeleteCategoryResponse, error)
 }
 
 type categoryServiceClient struct {
@@ -47,11 +55,55 @@ func (c *categoryServiceClient) CreateCategory(ctx context.Context, in *CreateCa
 	return out, nil
 }
 
+func (c *categoryServiceClient) ListCategories(ctx context.Context, in *ListCategoryRequest, opts ...grpc.CallOption) (*ListCategoryResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ListCategoryResponse)
+	err := c.cc.Invoke(ctx, CategoryService_ListCategories_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *categoryServiceClient) EditCategory(ctx context.Context, in *Category, opts ...grpc.CallOption) (*Category, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(Category)
+	err := c.cc.Invoke(ctx, CategoryService_EditCategory_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *categoryServiceClient) GetCategory(ctx context.Context, in *GetCategoryRequest, opts ...grpc.CallOption) (*Category, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(Category)
+	err := c.cc.Invoke(ctx, CategoryService_GetCategory_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *categoryServiceClient) DeleteCategory(ctx context.Context, in *DeleteCategoryRequest, opts ...grpc.CallOption) (*DeleteCategoryResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(DeleteCategoryResponse)
+	err := c.cc.Invoke(ctx, CategoryService_DeleteCategory_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CategoryServiceServer is the server API for CategoryService service.
 // All implementations must embed UnimplementedCategoryServiceServer
 // for forward compatibility.
 type CategoryServiceServer interface {
 	CreateCategory(context.Context, *CreateCategoryRequest) (*CreateCategoryResponse, error)
+	ListCategories(context.Context, *ListCategoryRequest) (*ListCategoryResponse, error)
+	EditCategory(context.Context, *Category) (*Category, error)
+	GetCategory(context.Context, *GetCategoryRequest) (*Category, error)
+	DeleteCategory(context.Context, *DeleteCategoryRequest) (*DeleteCategoryResponse, error)
 	mustEmbedUnimplementedCategoryServiceServer()
 }
 
@@ -64,6 +116,18 @@ type UnimplementedCategoryServiceServer struct{}
 
 func (UnimplementedCategoryServiceServer) CreateCategory(context.Context, *CreateCategoryRequest) (*CreateCategoryResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateCategory not implemented")
+}
+func (UnimplementedCategoryServiceServer) ListCategories(context.Context, *ListCategoryRequest) (*ListCategoryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListCategories not implemented")
+}
+func (UnimplementedCategoryServiceServer) EditCategory(context.Context, *Category) (*Category, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method EditCategory not implemented")
+}
+func (UnimplementedCategoryServiceServer) GetCategory(context.Context, *GetCategoryRequest) (*Category, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetCategory not implemented")
+}
+func (UnimplementedCategoryServiceServer) DeleteCategory(context.Context, *DeleteCategoryRequest) (*DeleteCategoryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteCategory not implemented")
 }
 func (UnimplementedCategoryServiceServer) mustEmbedUnimplementedCategoryServiceServer() {}
 func (UnimplementedCategoryServiceServer) testEmbeddedByValue()                         {}
@@ -104,6 +168,78 @@ func _CategoryService_CreateCategory_Handler(srv interface{}, ctx context.Contex
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CategoryService_ListCategories_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListCategoryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CategoryServiceServer).ListCategories(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CategoryService_ListCategories_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CategoryServiceServer).ListCategories(ctx, req.(*ListCategoryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CategoryService_EditCategory_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Category)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CategoryServiceServer).EditCategory(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CategoryService_EditCategory_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CategoryServiceServer).EditCategory(ctx, req.(*Category))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CategoryService_GetCategory_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetCategoryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CategoryServiceServer).GetCategory(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CategoryService_GetCategory_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CategoryServiceServer).GetCategory(ctx, req.(*GetCategoryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CategoryService_DeleteCategory_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteCategoryRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CategoryServiceServer).DeleteCategory(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CategoryService_DeleteCategory_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CategoryServiceServer).DeleteCategory(ctx, req.(*DeleteCategoryRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // CategoryService_ServiceDesc is the grpc.ServiceDesc for CategoryService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +250,22 @@ var CategoryService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CreateCategory",
 			Handler:    _CategoryService_CreateCategory_Handler,
+		},
+		{
+			MethodName: "ListCategories",
+			Handler:    _CategoryService_ListCategories_Handler,
+		},
+		{
+			MethodName: "EditCategory",
+			Handler:    _CategoryService_EditCategory_Handler,
+		},
+		{
+			MethodName: "GetCategory",
+			Handler:    _CategoryService_GetCategory_Handler,
+		},
+		{
+			MethodName: "DeleteCategory",
+			Handler:    _CategoryService_DeleteCategory_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- internal/app/services/category.service.go
+++ internal/app/services/category.service.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	pb "pft/main/internal/app/generated_proto"
 	"pft/main/internal/app/models"
+	"pft/main/internal/app/utils"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/jackc/pgx/v5/pgconn"
@@ -76,4 +77,139 @@ func (s *CategoryService) CreateCategory(ctx context.Context, req *pb.CreateCate
 		Message: "Category created successfully",
 		Data:    createdCategory,
 	}, nil
+}
+
+func (s *CategoryService) ListCategories(ctx context.Context, req *pb.ListCategoryRequest) (*pb.ListCategoryResponse, error) {
+	parsePagination, err := utils.ParsePaginationFilter(int(req.GetTake()), int(req.GetSkip()))
+	if err != nil {
+		return nil, err
+	}
+	take := parsePagination.Take
+	skip := parsePagination.Skip
+	userId := req.GetUserId()
+
+	if userId == "" {
+		return nil, fmt.Errorf("no user id provided")
+	}
+
+	var categories []*models.Category
+	result := s.db.
+		Where("user_id = ?", userId).
+		Offset(skip).Limit(take).Find(&categories)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	var total int64
+	s.db.Model(&models.Category{}).Where("user_id = ?", userId).Count(&total)
+
+	// Convert back to protobuf Accounts
+	var pbCategories []*pb.Category
+	for _, category := range categories {
+		pbCategory := &pb.Category{
+			Id:        category.Id.String(),
+			Name:      category.Name,
+			UserId:    category.UserId,
+			CreatedBy: category.CreatedBy,
+			CreatedAt: category.CreatedAt.String(),
+		}
+
+		pbCategories = append(pbCategories, pbCategory)
+	}
+
+	return &pb.ListCategoryResponse{Data: pbCategories, Total: total, Take: int32(take), Skip: int32(skip)}, nil
+}
+
+func (s *CategoryService) GetCategory(ctx context.Context, req *pb.GetCategoryRequest) (*pb.Category, error) {
+	id, err := utils.ValidateUUIDFromString(req.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	userId, err := utils.ValidateUUIDFromString(req.GetUserId())
+	if err != nil {
+		return nil, err
+	}
+
+	dbCategory := models.Category{}
+	result := s.db.Where("id = ? AND user_id = ?", id, userId).First(&dbCategory)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &pb.Category{
+		Id:        dbCategory.Id.String(),
+		Name:      dbCategory.Name,
+		UserId:    dbCategory.UserId,
+		CreatedBy: dbCategory.CreatedBy,
+		CreatedAt: dbCategory.CreatedAt.String(),
+	}, nil
+}
+
+func (s *CategoryService) EditCategory(ctx context.Context, req *pb.Category) (*pb.Category, error) {
+	id, err := utils.ValidateUUIDFromString(req.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	userId, err := utils.ValidateUUIDFromString(req.GetUserId())
+	if err != nil {
+		return nil, err
+	}
+
+	dbCategory := models.Category{}
+	result := s.db.
+		Where("id = ?", id).
+		Where("user_id = ?", userId).
+		First(&dbCategory)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	dbCategory.Name = req.GetName()
+
+	dbCategory.SetAuditUpdate(*userId)
+
+	result = s.db.Save(&dbCategory)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &pb.Category{
+		Id:        dbCategory.Id.String(),
+		Name:      dbCategory.Name,
+		UserId:    dbCategory.UserId,
+		CreatedAt: dbCategory.CreatedAt.String(),
+		CreatedBy: dbCategory.CreatedBy,
+	}, nil
+}
+
+func (s *CategoryService) DeleteCategory(ctx context.Context, req *pb.DeleteCategoryRequest) (*pb.DeleteCategoryResponse, error) {
+	id, err := utils.ValidateUUIDFromString(req.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	userId, err := utils.ValidateUUIDFromString(req.GetUserId())
+	if err != nil {
+		return nil, err
+	}
+
+	dbCategory := models.Category{}
+	result := s.db.Where("id = ? AND user_id = ?", id, userId).First(&dbCategory)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	result = s.db.Delete(&dbCategory)
+	if result.Error != nil {
+		if pgError := result.Error.(*pgconn.PgError); errors.Is(result.Error, pgError) {
+			if pgError.Code == "23503" && pgError.ConstraintName == "fk_transactions_category" {
+				return nil, status.Errorf(codes.InvalidArgument, "Cannot delete category that has existing transactions")
+			}
+		}
+		return nil, result.Error
+	}
+
+	return &pb.DeleteCategoryResponse{Message: "Category deleted successfully"}, nil
 }
